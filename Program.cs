01ec869c5bd5bdb86a6ac79046c0b68576fab5a2
EaMSo <emil@drsommerkrenz.de>
2023-05-16 21:35:23 +0200

    Upload der SQLite Datenbank und nichtfunktionierender erste Code der auf die Datenbank zugreifen soll sowie die .csproj Datei mit dem Includeten Package

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
+
+class Programm
+{
+    public static List<string> GetData()
+    {
+    var entries = new List<string>();
+    string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path,
+                                 "TUBAF_Planner.db");
+    using (var db = new SqliteConnection($"Filename={dbpath}"))
+    {
+        db.Open();
+        var selectCommand = new SqliteCommand
+            ("SELECT Text_Entry from MyTable", db);
+
+        SqliteDataReader query = selectCommand.ExecuteReader();
+
+        while (query.Read())
+        {
+            entries.Add(query.GetString(0));
+        }
+    }
+
+    return entries;
+    }
+}
+
