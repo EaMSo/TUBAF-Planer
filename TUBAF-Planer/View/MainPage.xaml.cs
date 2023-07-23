776f2249d9f0974596f8ef96c357e9835f24168e
Jannik John <Jannik.John3@web.de>
2023-07-23 16:58:26 +0200

    Custom Modul einlesbar

--- TUBAF-Planer/View/MainPage.xaml.cs
+++ TUBAF-Planer/View/MainPage.xaml.cs
@@ -19,7 +19,7 @@ public partial class MainPage : ContentPage
 		
 
 		CustomModule.CreateTable();
-
+		/*
 		List<Modul> fullmodulelist = FullmoduleList.GetFullmoduleList();
 		string modulstring = "";
 		foreach(Modul modul in fullmodulelist)
@@ -28,7 +28,7 @@ public partial class MainPage : ContentPage
 		}
 		
 		But.Text = modulstring;
-		
+		*/
 
     }
 
